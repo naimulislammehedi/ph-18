54b2d0a9ea7cfaabf50b56872d3b39a0f012d0f9
Mehedi <mehedi.interface@gmail.com>
2026-05-03 22:47:01 +0600

    18-3 Check whether a year is a Leap Year or not

---
+++ leap_year.js
@@ -0,0 +1,31 @@
+/* Year will be a leap year if the year is divisible by 4 */
+function isLeapYear(year) {
+    if (year % 4 === 0) {
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
+const isLipi = isLeapYear(2043); 
+console.log(isLipi); 
+
+/*
+1. those year that is not divisible by 100, if the year is divisible by 4: then it will be a leap year
+2.  
+*/
+
+function isLeapYear2(year) {
+    if(year % 100 !== 0 && year % 4 === 0) {
+        return true; 
+    } else if (year % 100 == 0 && year % 400 == 0) {
+        return true
+    } else {
+        return false; 
+    }
+}
+
+const isLeap = isLeapYear2(2100); 
+const isLeap2 = isLeapYear2(2400); 
+const isLeap3 = isLeapYear2(1900); 
+console.log(isLeap, isLeap2, isLeap3); 
