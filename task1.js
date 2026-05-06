c6fb86d1022350c7fdd206f3d6bb43f1635d3dd7
Mehedi <mehedi.interface@gmail.com>
2026-05-06 07:46:52 +0600

    Task-1

---
+++ task1.js
@@ -0,0 +1,8 @@
+// Task-1: Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 1.8) + 32; 
+}
+
+const temperature = celsiusToFahrenheit(10); 
+console.log(temperature); 
